3bcc471ce7bdcff1e832d11f17c803acf4e402db
Viet Le <vietle@SVUSLP01019.attlocal.net>
2021-01-27 15:49:46 -0500

    added POST and GET for swagger

---
+++ docs/doc.go
@@ -0,0 +1,17 @@
+// Package classification inventory manager
+//
+// Documentation of our inventory API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: shasta-inventory-manager-dev.com
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
+package docs
---
+++ docs/inventory_manager_swagger.go
@@ -0,0 +1,32 @@
+package docs
+
+import "github.com/onelzyugy/projects/golang-microservice/types"
+
+// swagger:route POST /order order bubbleTeaRequest
+// oder bubble tea drinks.
+// responses:
+//   200: bubbleTeaResponse
+//	 500: bubbleTeaResponse
+
+// swagger:parameters bubbleTeaRequest
+type bubbleTeaRequestWrapper struct {
+	// in:body
+	Body types.BubbleTeaRequest
+}
+
+// swagger:response bubbleTeaResponse
+type bubbleTeaResponseWrapper struct {
+	// in:body
+	Body types.BubbleTeaResponse
+}
+
+// swagger:route GET /health-check health check
+// health check of the app
+// responses:
+//	 200: healthResponse
+
+// swagger:response healthResponse
+type healthResponseWrapper struct {
+	// in:body
+	Body types.Health
+}
